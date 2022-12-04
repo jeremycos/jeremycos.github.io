9922ec8a90d4b394b4c8bce0dc3bf4647d90d8b4
Jeremcode <73873125+jeremycos@users.noreply.github.com>
2022-12-04 12:09:05 +0200

    Create Hero.js

---
+++ generativ/Hero.js
@@ -0,0 +1,34 @@
+<div className="text-center bg-gradient-to-b from-white to-zinc-50 w-full  mx-auto animate__animated animate__fadeInUp  px-4 pt-24 sm:pt-36 sm:pb-24 pb-24 ">
+  <div className="max-w-5xl relative mx-auto">
+    <h1 className="text-4xl  font-extrabold text-gray-900 sm:text-4xl md:text-6xl">
+      <span className="block xl:inline leading-tight">
+        $title
+        <sup className="absolute top-0 right-0 -mt-3 md:-mt-12 mr-3 md:mr-0 ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            fill="currentColor"
+            className="bi bi-stars h-5 w-6 md:h-auto md:w-auto"
+            viewBox="0 0 16 16"
+          >
+            <path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828l.645-1.937zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.734 1.734 0 0 0 4.593 5.69l-.387 1.162a.217.217 0 0 1-.412 0L3.407 5.69A1.734 1.734 0 0 0 2.31 4.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.734 1.734 0 0 0 3.407 2.31l.387-1.162zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732L9.1 2.137a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L10.863.1z"></path>
+          </svg>
+        </sup>
+      </span>
+    </h1>
+    <p className="mx-auto text-lg font-normal leading-tight text-gray-700 sm:text-lg mt-8 md:mt-8 md:text-xl max-w-md md:max-w-4xl">
+      $subtitle
+    </p>
+    <div className="mt-10 inline-block md:max-w-md mx-auto sm:flex sm:justify-center md:mt-12">
+      <div className="rounded-md shadow">
+        <a
+          href="$buttonLink"
+          className=" bg-gradient-to-r from-green-400 to-blue-500 hover:from-gray-900 hover:to-gray-900  flex items-center justify-center px-8 py-3 font-normal tracking-wide rounded-md text-white  md:py-3 text-lg md:px-16"
+        >
+          $buttonText
+        </a>
+      </div>
+    </div>
+  </div>
+</div>;
